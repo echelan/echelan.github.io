739374f3d582926c88de72e3c969dc10cab06f82
Echelan <andres_movilla1103@hotmail.com>
2018-05-29 01:31:06 -0500

    main index done, maybe?
    missing information

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 (function($){
   $(function(){
 
-    
+    // KIM POSSIBLE REFERENCE
+    var k = $("#k");
+    var j = ["CALL ME","BEEP ME","REACH ME"];
+    var i = Math.floor(Math.random()*10);
+    k.text(j[i]);
+
+    // HEAD SPIN
+    var h = $("#h");
+    h.hover(
+      function() {
+        h.css("transform","rotate(360deg)");
+      },
+      function() {
+        h.css("transform","rotate(0deg)");
+      }
+    );
 
   });
 })(jQuery);
