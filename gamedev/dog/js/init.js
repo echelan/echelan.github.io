69e3851c03f72dfa5133f8fc8640d2ed51157ccf
Echelan <andres_movilla1103@hotmail.com>
2018-08-01 16:42:14 -0500

    finished gamedev branch

--- gamedev/dog/js/init.js
+++ gamedev/dog/js/init.js
@@ -1,6 +1,9 @@
 (function($){
   $(function(){
+
+    // PARALLAX
     $('.parallax').parallax();
-    $('.carousel').carousel({indicators: true});
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+
+
+  });
+})(jQuery);
